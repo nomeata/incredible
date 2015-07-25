38f0c6ec070eb4e8f3ec7eeac77b0997d39307dd
Joachim Breitner <mail@joachim-breitner.de>
2015-07-25 15:38:16 +0200

    Gibhub Pages: Include npm dependencies

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -2,15 +2,24 @@
 
 # inspired by https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 
-make
-
+# Prepare an empty directory
 rm -rf gh-page
 mkdir gh-page
 cd gh-page
 git init
 git config user.name "Incredible CI"
 git config user.email "mail@joachim-breitner.de"
+
+
+# Make sure the project is built
+make -C ..
+
+# Copy static HTML files and generated JS files
 cp -v ../*.html ../*.js .
+
+# Install node libraries
+npm install --production ..
+
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
