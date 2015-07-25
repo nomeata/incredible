fb6fbcbf4bf4d62c93a7d2b697489742b2e2f152
Joachim Breitner <mail@joachim-breitner.de>
2015-07-25 10:18:50 +0200

    New command bundle-examples
    which takes all the examples logics, tasks and proofs from the examples
    directory, and bundles them in one JSON file, for consumption by the UI.

---
+++ logic/Examples.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
+module Examples where
+
+import qualified Data.Map as M
+import Data.Yaml
+import System.Directory.Extra
+import Control.Applicative
+import System.FilePath
+import Control.Monad
+import qualified Data.ByteString as B
+
+
+data Examples = Examples
+ { exampleLogics :: M.Map String Value
+ , exampleTasks  :: M.Map String Value
+ , exampleProofs :: M.Map String Value
+ }
+
+instance ToJSON Examples where
+  toJSON (Examples {..}) = object
+    [ "logics" .= exampleLogics
+    , "tasks"  .= exampleTasks
+    , "proofs" .= exampleProofs
+    ]
+
+readDirectoryOfYamlFiles :: FilePath -> IO (M.Map String Value)
+readDirectoryOfYamlFiles dir = do
+  files <- listFiles dir
+  let yamlFiles = filter ((".yaml" == ) . takeExtension) files
+  entries <- forM yamlFiles $ \f -> do
+    content <- B.readFile f
+    let value = either error id $ decodeEither content
+    return (dropExtension (takeFileName f), value)
+  return $ M.fromList entries
+
+readExamples :: FilePath -> IO Examples
+readExamples dir = Examples <$> readDirectoryOfYamlFiles (dir </> "logics")
+                            <*> readDirectoryOfYamlFiles (dir </> "tasks")
+                            <*> readDirectoryOfYamlFiles (dir </> "proofs")
