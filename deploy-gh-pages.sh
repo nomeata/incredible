4f4dcdc5cf3633d3669aacd3d3bbabd4886f4848
Martin Mohr <martin.mohr@kit.edu>
2015-08-01 14:29:10 +0200

    revert accidental destruction of deploy script

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -35,6 +35,6 @@ cd gh-page
 
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://github.com/nomeata/incredible-demo.git" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
 
