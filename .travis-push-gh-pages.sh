1fcf420eb3c975b4943ee095e03465d28f0eafa4
Joachim Breitner <mail@joachim-breitner.de>
2015-08-02 11:49:50 +0200

    Add github pages upload script
    (ommitted by accident in 0f19598).

---
+++ .travis-push-gh-pages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# inspired by https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+
+./deploy.sh gh-page
+
+# Prepare an empty directory
+cd gh-page
+git init
+git config user.name "Incredible CI"
+git config user.email "mail@joachim-breitner.de"
+git add .
+git commit -m "Deploy to GitHub Pages"
+# The diversion to /dev/null is required to keep the GH_TOKEN secret
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+
