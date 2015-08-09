72e44f7288fb8f4f8e17d01b5487216cb3fb45c1
Joachim Breitner <mail@joachim-breitner.de>
2015-08-09 12:09:03 +0200

    Use unicode operators for propositions in the logic
    with backward compatible parsing of and(,) etc. The pretty-printer is
    not yet pretty (too many parenthesis), maybe I should use the
    "invertible-syntax" package.

--- logic/Propositions.hs
+++ logic/Propositions.hs
@@ -1,7 +1,13 @@
 module Propositions where
 
 import Text.Parsec
-import Control.Applicative
+import Text.Parsec.Expr
+import Text.Parsec.String
+import Data.Functor.Identity
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Control.Applicative hiding ((<|>))
+import Data.Maybe
 import Data.List
 import Data.Void
 
@@ -11,27 +17,71 @@ data Term f v = Symb f [Term f v] | Var v
 
 type Proposition = Term String String
 
+abbreviations :: M.Map String String
+abbreviations = M.fromList
+    [ ("and", "∧")
+    , ("or", "∨")
+    , ("imp", "→")
+    , ("True", "⊤")
+    , ("False", "⊥")
+    ]
+
+infixSymbols :: S.Set String
+infixSymbols = S.fromList $ words "∧ ∨ →"
+
 mapVar :: (v1 -> v2) -> Term f v1 -> Term f v2
 mapVar vf (Symb f ts) = Symb f $ map (mapVar vf) ts
 mapVar vf (Var v) = Var $ vf v
+
+-- Pretty printer
 
 -- Horribly inefficient, all that
 printTerm :: Proposition -> String
 printTerm (Var v) = v
 printTerm (Symb f []) = f
-printTerm (Symb f args) = f ++ "(" ++ intercalate "," (map printTerm args) ++ ")"
+printTerm (Symb f [arg1,arg2])
+    | f `S.member` infixSymbols
+    = printParens (printTerm arg1) ++ f ++ printParens (printTerm arg2)
+printTerm (Symb f args) = f ++ printParens (intercalate "," (map printTerm args))
+
+printParens :: String -> String
+printParens s = "("++s++")"
+
+-- Parser
 
 parseTerm :: String -> Either String Proposition
-parseTerm = either (Left . show) Right . parse termP ""
+parseTerm = either (Left . show) Right . parse (between (return ()) eof termP) ""
 
-termP :: Parsec String () Proposition
-termP = do
-    hd <- many1 alphaNum
-    if hd == "False"
-    then return $ Symb "False" []
-    else option (Var hd) $ do
-        between (char '(') (char ')') $ do
-            Symb hd <$> termP `sepBy1` (char ',')
+-- lexeme
+l :: Parser a -> Parser a
+l = between (return ()) spaces
+
+termP :: Parser Proposition
+termP =  buildExpressionParser table atomP <?> "proposition"
+
+table :: OperatorTable String () Identity Proposition
+table = [ [ binary "∧" ["&"]
+          , binary "→" ["->"]
+          ]
+        , [ binary "∨" ["|"]
+          ]
+        ]
+  where
+    binary op alts = Infix ((\a b -> Symb op [a,b]) <$ l (choice (map string (op:alts)))) AssocLeft
+
+atomP :: Parser Proposition
+atomP = choice
+    [ string "⊥" >> return (Symb "⊥" [])
+    , string "⊤" >> return (Symb "⊤" [])
+    , try (string "False") >> return (Symb "⊥" [])
+    , try (string "True") >> return (Symb "⊤" [])
+    , between (char '(') (char ')') termP
+    , do
+        hd <- many1 alphaNum
+        let sym = fromMaybe hd $ M.lookup hd abbreviations
+        option (Var sym) $ between (char '(') (char ')') $ do
+            Symb sym <$> termP `sepBy1` (char ',')
+    ]
 
 
 -- Ground terms are terms with no variables, such as the propositions in the
@@ -39,14 +89,8 @@ termP = do
 type GroundTerm = Term String Void
 
 parseGroundTerm :: String -> Either String GroundTerm
-parseGroundTerm = either (Left . show) Right . parse groundTermP ""
+parseGroundTerm = either (Left . show) (Right . fixVar) . parse termP ""
 
-
-groundTermP :: Parsec String () GroundTerm
-groundTermP = do
-    hd <- many1 alphaNum
-    args <- option [] $ do
-        between (char '(') (char ')') $ do
-            groundTermP `sepBy1` (char ',')
-    return $ Symb hd args
-
+fixVar :: Term a a -> Term a Void
+fixVar (Symb f ts) = Symb f $ map fixVar ts
+fixVar (Var v) = Symb v []
