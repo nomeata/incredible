315f8f80394c37ed50bb610fb93fcb4cb58b5c46
Richard Molitor <gattschardo@gmail.com>
2015-09-18 19:54:32 +0200

    Move unification from LabelConnections to new module Analysis

---
+++ logic/Analysis.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE TupleSections,FlexibleContexts #-}
+module Analysis where
+
+import Data.Functor
+import qualified Data.Map as M
+import Data.Map ((!))
+import Data.Tagged -- because unbound does not handle Tag :-(
+import Control.Arrow
+
+import Types
+import Unification
+import Propositions
+import Scopes
+
+import Unbound.LocallyNameless
+import Unbound.LocallyNameless.Fresh
+
+type BlockProps = M.Map (Key Block) (M.Map (Key Port) Term)
+
+prepare :: Context -> Task -> Proof -> (BlockProps, [Var])
+prepare ctxt task proof =
+    (M.mapWithKey prepareBlock renamedBlockMap, unificationVariables)
+  where
+    -- Strategy:
+    --  1. For each block in the proof, create a data structure
+    --     (Bind [block-local var]  ([unification variabes], [(port name, proposition)]))
+    --     (a.k.a BlockData)  to abstract over these.
+    --  2. Apply unbind to each of them, to get unique names for them:
+    --     [(port name, proposition)]
+    --  3. Calculate scopes, and give scoped bound variables as arguments to
+    --     free variables
+    --  3. Look at each connection, turn this into equations
+    --     [(proposition, proposition)], and pass them to the unification module
+    --  4. Get back a substiution, apply it to the data structure from step 2
+    --  5. For each connection, calculate the ConnLabel
+
+    closedBlockData :: [(Key Block, Bind [Var] ([Var], [(String, (Term, [Var]))]))]
+    closedBlockData =
+        [ (blockKey, bind l (f, portList))
+        | (blockKey, block) <- M.toList (blocks proof)
+        , let rule = block2Rule ctxt block
+              l = localVars rule
+              f = freeVars rule
+              portList =
+                [ (untag pk, (portProp p, portScopes p))
+                | (pk,p) <- M.toList $ ports rule
+                ]
+        ]
+
+    -- Is it ok to limit runFreshM to this, or does the whole function have to
+    -- live in FreshM?
+    renamedBlockData :: [(Key Block, ([Var], M.Map (Key Port) (Term, [Var])))]
+    renamedBlockData = flip contFreshM i $
+         mapM go closedBlockData
+      where
+        i = firstFree $ map snd closedBlockData
+        go (blockKey, boundBlockData) = do
+            (_,(uvs,portList)) <- unbind boundBlockData
+            return (blockKey, (uvs, M.fromList (map (first Tagged) portList)))
+    renamedBlockMap = M.fromList renamedBlockData
+
+    scopedVariables :: [Var]
+    scopedVariables = concatMap (concatMap snd . M.elems . snd . snd) renamedBlockData
+
+    scopes = calculateScopes ctxt task proof
+    scopeMap = M.fromListWith (++) [ (k, [pdom]) | (ks, pdom) <- scopes, k <- ks ]
+
+
+    prepareBlock blockKey (unv, ports) = M.map preparePort ports
+      where
+        scopedVars = [ v
+            | BlockPort pdBlockKey pdPortKey <- M.findWithDefault [] blockKey scopeMap
+            , Just (_,ports) <- return $ M.lookup pdBlockKey renamedBlockMap
+            , let (_,sv) = ports M.! pdPortKey
+            , v <- sv
+            ]
+        -- Change free variables to variables, possibly depending on these arguments
+        s = [ (s, mkApps (V s) (map V scopedVars)) | s <- unv ] ++
+            [ (s, V s) | s <- scopedVariables ]
+        preparePort (prop, _) = (substs s prop)
+
+    unificationVariables :: [Var]
+    unificationVariables = concat $ map (fst.snd) renamedBlockData
+
+
+analyse :: Task -> Proof -> BlockProps -> [Var] -> (Bindings, [(Key Connection, UnificationResult)])
+analyse task proof renamedBlockProps unificationVariables =
+    (final_bind, unificationResults)
+  where
+    equations =
+        [ (connKey, (prop1, prop2))
+        | (connKey, conn) <- M.toList (connections proof)
+        , Just prop1 <- return $ propAt task renamedBlockProps (connFrom conn)
+        , Just prop2 <- return $ propAt task renamedBlockProps (connTo conn)
+        ]
+
+    (final_bind, unificationResults) = unifyLiberally unificationVariables equations
+
+propAt :: Task -> BlockProps -> PortSpec -> Maybe Proposition
+propAt _ _ NoPort = Nothing
+propAt task _ (ConclusionPort n) = Just $ tConclusions task !! (n-1)
+propAt task _ (AssumptionPort n) = Just $ tAssumptions task !! (n-1)
+propAt _ blockProps (BlockPort blockKey portKey) = Just $ blockProps ! blockKey ! portKey
--- logic/LabelConnections.hs
+++ logic/LabelConnections.hs
@@ -1,106 +1,23 @@
 {-# LANGUAGE TupleSections,FlexibleContexts #-}
 module LabelConnections where
 
-import Data.Functor
 import qualified Data.Map as M
-import Data.Map ((!))
-import Data.Tagged -- because unbound does not handle Tag :-(
---import Debug.Trace
-import Control.Arrow
 
 import Types
 import Unification
-import Propositions
-import Scopes
-
-import Unbound.LocallyNameless
-import Unbound.LocallyNameless.Fresh
+import Analysis
 
 
 labelConnections :: Context -> Task -> Proof -> M.Map (Key Connection) ConnLabel
 labelConnections ctxt task proof =
     M.mapWithKey instantiate (connections proof)
   where
-    -- Strategy:
-    --  1. For each block in the proof, create a data structure
-    --     (Bind [block-local var]  ([unification variabes], [(port name, proposition)]))
-    --     (a.k.a BlockData)  to abstract over these.
-    --  2. Apply unbind to each of them, to get unique names for them:
-    --     [(port name, proposition)]
-    --  3. Calculate scopes, and give scoped bound variables as arguments to
-    --     free variables
-    --  3. Look at each connection, turn this into equations
-    --     [(proposition, proposition)], and pass them to the unification module
-    --  4. Get back a substiution, apply it to the data structure from step 2
-    --  5. For each connection, calculate the ConnLabel
+    (renamedBlockProps, unificationVariables) = prepare ctxt task proof
 
-    closedBlockData :: [(Key Block, Bind [Var] ([Var], [(String, (Term, [Var]))]))]
-    closedBlockData =
-        [ (blockKey, bind l (f, portList))
-        | (blockKey, block) <- M.toList (blocks proof)
-        , let rule = block2Rule ctxt block
-              l = localVars rule
-              f = freeVars rule
-              portList =
-                [ (untag pk, (portProp p, portScopes p))
-                | (pk,p) <- M.toList $ ports rule
-                ]
-        ]
-
-    -- Is it ok to limit runFreshM to this, or does the whole function have to
-    -- live in FreshM?
-    renamedBlockData :: [(Key Block, ([Var], M.Map (Key Port) (Term, [Var])))]
-    renamedBlockData = flip contFreshM i $
-         mapM go closedBlockData
-      where
-        i = firstFree $ map snd closedBlockData
-        go (blockKey, boundBlockData) = do
-            (_,(uvs,portList)) <- unbind boundBlockData
-            return (blockKey, (uvs, M.fromList (map (first Tagged) portList)))
-    renamedBlockMap = M.fromList renamedBlockData
-
-    unificationVariables :: [Var]
-    unificationVariables = concat $ map (fst.snd) renamedBlockData
-
-    scopedVariables :: [Var]
-    scopedVariables = concatMap (concatMap snd . M.elems . snd . snd) renamedBlockData
-
-    scopes = calculateScopes ctxt task proof
-    scopeMap = M.fromListWith (++) [ (k, [pdom]) | (ks, pdom) <- scopes, k <- ks ]
-
-    renamedBlockProps :: M.Map (Key Block) (M.Map (Key Port) Term)
-    renamedBlockProps = M.mapWithKey prepareBlock renamedBlockMap
-
-    prepareBlock blockKey (unv, ports) = M.map preparePort ports
-      where
-        scopedVars = [ v
-            | BlockPort pdBlockKey pdPortKey <- M.findWithDefault [] blockKey scopeMap
-            , Just (_,ports) <- return $ M.lookup pdBlockKey renamedBlockMap
-            , let (_,sv) = ports M.! pdPortKey
-            , v <- sv
-            ]
-        -- Change free variables to variables, possibly depending on these arguments
-        s = [ (s, mkApps (V s) (map V scopedVars)) | s <- unv ] ++
-            [ (s, V s) | s <- scopedVariables ]
-        preparePort (prop, _) = (substs s prop)
-
-    propAt NoPort                       = Nothing
-    propAt (ConclusionPort n)           = Just $ tConclusions task !! (n-1)
-    propAt (AssumptionPort n)           = Just $ tAssumptions task !! (n-1)
-    propAt (BlockPort blockKey portKey) = Just $ renamedBlockProps ! blockKey ! portKey
-
-    equations =
-        [ (connKey, (prop1, prop2))
-        | (connKey, conn) <- M.toList (connections proof)
-        , Just prop1 <- return $ propAt (connFrom conn)
-        , Just prop2 <- return $ propAt (connTo conn)
-        ]
-
-    (final_bind, unificationResults) = unifyLiberally unificationVariables equations
+    (final_bind, unificationResults) = analyse task proof renamedBlockProps unificationVariables
 
     resultsMap :: M.Map (Key Connection) UnificationResult
     resultsMap = M.fromList unificationResults
-
 
     instantiate connKey conn = case (propFromMB, propToMB) of
         (Nothing, Nothing) -> Unconnected
@@ -112,5 +29,5 @@ labelConnections ctxt task proof =
             Dunno  -> DunnoLabel propFrom propTo
             Failed -> Mismatch propFrom propTo
       where
-        propFromMB = applyBinding final_bind <$> propAt (connFrom conn)
-        propToMB   = applyBinding final_bind <$> propAt (connTo conn)
+        propFromMB = applyBinding final_bind <$> propAt task renamedBlockProps (connFrom conn)
+        propToMB   = applyBinding final_bind <$> propAt task renamedBlockProps (connTo conn)
