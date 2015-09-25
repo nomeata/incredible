nomeata/incredible

commit 2cc6f081f51e22a23a231d032108686adc1c22ee

Apply some object orientation to shapes.js
with the prospect of de-duplicating the updateSizes() functions, and
easier introduce further shapes, e.g. for #10.

webui/incredible-elements.js
@@ -64,7 +64,7 @@ joint.shapes.incredible.GenericView = joint.dia.ElementView.extend({
   },
 
   renderMarkup: function() {
-    renderBlockDescToSVG(this.vel, this.getBlockDesc(), true);
+    BlockDescRenderer(this.vel, this.getBlockDesc(), true).renderToSVG();
 
     var cellView = this;
     function resizeSchieblehre(e) {
@@ -98,7 +98,7 @@ joint.shapes.incredible.GenericView = joint.dia.ElementView.extend({
   },
 
   updateSizes: function () {
-    updateSizes(this.vel, this.getBlockDesc());
+    BlockDescRenderer(this.vel, this.getBlockDesc(), true).updateSizes();
   },
 
   update: function () {

webui/webui.js
@@ -47,7 +47,7 @@ function renderBlockDescToDraggable(blockDesc, container) {
 
   var g = V("<g/>");
   vel.append(g);
-  renderBlockDescToSVG(g, blockDesc, false);
+  BlockDescRenderer(g, blockDesc, false).renderToSVG();
   gBB = g.bbox(false);
   g.translate($(el).width()/2, -gBB.y + 5);
 

commit ecbc862e342942896e552b94c51c560cf8ea7187

Fix call to updateSizes
this is fallout from 2cc6f081 and fixes #35.

webui/incredible-elements.js
@@ -128,7 +128,7 @@ joint.shapes.incredible.GenericView = joint.dia.ElementView.extend({
       var text = "✎"+this.model.get('annotation');
       if (textV.text() != text) {
         textV.text(text);
-        updateSizes(this.vel, this.getBlockDesc());
+        this.updateSizes();
       }
     }
 