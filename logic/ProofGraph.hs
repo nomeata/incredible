ed688c176c24e261436c3ae75c7186a3c92a352b
Joachim Breitner <mail@joachim-breitner.de>
2015-10-04 15:10:47 +0200

    Implement better scoping logic
    this fixes #54, and replaces the use of dom-lt by some custom code in
    ProofGraph.
    
    Note how pretty the traversals `calcNonScope` and `calcSCC`
    are once the `MarkM` monad is defined, which (together with `execMarkM`
    and `markAndFollow` is also very concise. Yay for monads!

--- logic/ProofGraph.hs
+++ logic/ProofGraph.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, TypeFamilies, RecordWildCards, FlexibleContexts #-}
+{-# LANGUAGE DataKinds, TypeFamilies, RecordWildCards, FlexibleContexts, StandaloneDeriving, FlexibleInstances, GADTs, ExistentialQuantification #-}
 
 -- |
 -- The proof is (natuarlly) a directed graph. For full detail, one can view
@@ -11,12 +11,22 @@
 module ProofGraph where
 
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.Map ((!))
 import Data.List
+import Data.Maybe
+import Control.Monad
+import Control.Monad.Trans.State.Strict
 
 import Types
 
 data NodeType = BlockNodeType | OutPortNodeType | InPortNodeType | ConnNodeType
+
+data NodeTag a where
+    BlockNodeTag :: NodeTag 'BlockNodeType
+    OutPortNodeTag :: NodeTag 'OutPortNodeType
+    InPortNodeTag :: NodeTag 'InPortNodeType
+    ConnNodeTag :: NodeTag 'ConnNodeType
 
 type family Succ a where
     Succ 'BlockNodeType   = 'OutPortNodeType
@@ -36,11 +46,27 @@ type family NodeKey a where
     NodeKey 'InPortNodeType  = PortSpec
     NodeKey 'ConnNodeType    = Key Connection
 
+data ANodeKey
+    = BlockNodeKey   (NodeKey BlockNodeType)
+    | OutPortNodeKey (NodeKey OutPortNodeType)
+    | InPortNodeKey  (NodeKey InPortNodeType)
+    | ConnNodeKey    (NodeKey ConnNodeType)
+    deriving (Eq, Ord)
+
 data Node a = Node
-    { nodeKey :: NodeKey a
+    { nodeType :: NodeTag a
+    , nodeKey  :: NodeKey a
     , nodePred :: [Node (Pred a)]
     , nodeSucc :: [Node (Succ a)]
     }
+
+node2ANodeKey :: Node a -> ANodeKey
+node2ANodeKey (Node BlockNodeTag k _ _) = BlockNodeKey k
+node2ANodeKey (Node InPortNodeTag k _ _) = InPortNodeKey k
+node2ANodeKey (Node OutPortNodeTag k _ _) = OutPortNodeKey k
+node2ANodeKey (Node ConnNodeTag k _ _) = ConnNodeKey k
+
+data ANode = forall a. ANode (Node a)
 
 type NodeMap a = M.Map (NodeKey a) (Node a)
 
@@ -60,7 +86,8 @@ proof2Graph ctxt proof = Graph {..}
     blockNodes = mkNodeMap $ map go $ M.toList (blocks proof)
       where
         go (blockKey, block) =
-            Node blockKey
+            Node BlockNodeTag
+                 blockKey
                  [inPortNodes  ! (BlockPort blockKey portKey) | (portKey,_) <- inPorts]
                  [outPortNodes ! (BlockPort blockKey portKey) | (portKey,_) <- outPorts]
           where
@@ -69,7 +96,7 @@ proof2Graph ctxt proof = Graph {..}
               M.toList $ ports (block2Rule ctxt block)
 
     outPortNodes = mkNodeMap
-        [ Node ps [blockNodes ! blockKey] succs
+        [ Node OutPortNodeTag ps [blockNodes ! blockKey] succs
         | (blockKey, block) <- M.toList (blocks proof)
         , let rule = block2Rule ctxt block
         , (portKey, port) <- M.toList (ports rule)
@@ -86,7 +113,7 @@ proof2Graph ctxt proof = Graph {..}
         ]
 
     inPortNodes = mkNodeMap
-        [ Node ps preds [blockNodes ! blockKey]
+        [ Node InPortNodeTag ps preds [blockNodes ! blockKey]
         | (blockKey, block) <- M.toList (blocks proof)
         , let rule = block2Rule ctxt block
         , (portKey, port) <- M.toList (ports rule)
@@ -103,8 +130,58 @@ proof2Graph ctxt proof = Graph {..}
         ]
 
     connectionNodes = mkNodeMap
-        [ Node connKey
+        [ Node ConnNodeTag
+               connKey
                [outPortNodes ! ps | Just ps <- return $ connFrom conn]
                [inPortNodes ! ps  | Just ps <- return $ connTo conn]
         | (connKey, conn) <- M.toList (connections proof)
         ]
+
+
+calcNonScope :: Node a -> S.Set ANodeKey -> S.Set ANodeKey
+calcNonScope start stopNodes = execMarkM $ goForward start
+  where
+    goForward :: Node a -> MarkM ()
+    goForward n | node2ANodeKey n `S.member` stopNodes = return ()
+    goForward n = markAndFollow (node2ANodeKey n) $ do
+        mapM_ goForward  (nodeSucc n)
+        mapM_ goBackward (nodePred n)
+
+    goBackward :: Node a -> MarkM ()
+    goBackward n | node2ANodeKey n `S.member` stopNodes = return ()
+    goBackward n = markAndFollow (node2ANodeKey n) $ do
+        mapM_ goBackward (nodePred n)
+
+
+calcSCC :: Node a -> S.Set ANodeKey
+calcSCC start = execMarkM $ go start
+  where
+    go :: Node a -> MarkM ()
+    go n = markAndFollow (node2ANodeKey n) $ do
+        mapM_ go (nodePred n)
+        mapM_ go (nodeSucc n)
+
+calcScope :: Graph -> Key Block -> [PortSpec] -> [PortSpec] -> [Key Block]
+calcScope Graph{..} start stopAtIn stopAtOut =
+    mapMaybe toBlockNodeKey $
+    S.toList $
+    calcSCC node `S.difference` calcNonScope node stopNodes
+  where
+    node = blockNodes ! start
+    stopNodes = S.fromList (map InPortNodeKey stopAtIn) `S.union`
+                S.fromList (map OutPortNodeKey stopAtOut)
+
+toBlockNodeKey :: ANodeKey -> Maybe (Key Block)
+toBlockNodeKey (BlockNodeKey k) = Just k
+toBlockNodeKey _ = Nothing
+
+type MarkM = State (S.Set ANodeKey)
+execMarkM :: MarkM a -> S.Set ANodeKey
+execMarkM a = execState a S.empty
+
+markAndFollow :: ANodeKey -> MarkM () -> MarkM ()
+markAndFollow k a = do
+    seen <- gets (k `S.member`)
+    unless seen $ do
+        modify (S.insert k)
+        a
--- logic/Scopes.hs
+++ logic/Scopes.hs
@@ -2,83 +2,25 @@
 module Scopes where
 
 import qualified Data.Map as M
-import qualified Data.IntMap as IM
-import qualified Data.IntSet as IS
-import Control.Monad.Trans.Writer
-import Control.Arrow
-import Control.Monad
-import Data.Tuple
-import Data.Tree
-
-import Data.Graph.Dom
 
 import Types
+import ProofGraph
 
 type Scope = ([Key Block], PortSpec)
 
 calculateScopes :: Context -> Proof -> [Scope]
-calculateScopes ctxt proof = scopes
+calculateScopes ctxt proof =
+    [ (calcScope graph blockKey [ps] allLocalHyps, ps)
+    | (blockKey, block) <- M.toList (blocks proof)
+    , let rule = block2Rule ctxt block
+    , (portKey, Port {portType = PTAssumption, portScopes = _:_}) <- M.toList (ports rule)
+    , let ps = BlockPort blockKey portKey
+    ]
   where
-    -- Building a graph for the dom-lt library
-
-    exitNode = 0 :: Node
-
-    -- We only need to consider assumption ports
-    portSpecs =
-        [ BlockPort blockKey portKey
+    graph = proof2Graph ctxt proof
+    allLocalHyps =
+        [ BlockPort blockKey hypKey
         | (blockKey, block) <- M.toList (blocks proof)
         , let rule = block2Rule ctxt block
-        , (portKey, Port {portType = PTAssumption}) <- M.toList (ports rule)
+        , (hypKey, Port {portType = PTLocalHyp{}}) <- M.toList (ports rule)
         ]
-
-    (node2obj, obj2node) =
-        IM.fromList &&& (M.fromList . map swap) $
-        zip [1..] $
-        (map Left (M.keys (blocks proof))) ++ (map Right portSpecs)
-
-    graph :: Graph
-    graph = IM.fromList $
-        [ (obj2node M.! (Left blockKey), findSuccs blockKey) | blockKey <- M.keys (blocks proof) ] ++
-        [ (obj2node M.! (Right ps), IS.singleton (obj2node M.! Left blockKey)) | ps@(BlockPort blockKey _) <- portSpecs ] ++
-        [ (exitNode, IS.empty) ]
-
-    findSuccs :: Key Block -> IS.IntSet
-    findSuccs blockKey = IS.fromList $ fakeExit $ map portSpec2Node $ connsFrom blockKey
-
-    portSpec2Node :: PortSpec -> Node
-    portSpec2Node ps = obj2node M.! Right ps
-
-    fakeExit :: [Node] -> [Node]
-    fakeExit [] = [exitNode]
-    fakeExit succs = succs
-
-    fromMap = M.fromListWith (++)
-        [ (fromBlock, [ps])
-        | Connection {connFrom = Just (BlockPort fromBlock _), connTo = Just ps}
-            <- M.elems $ connections proof]
-    connsFrom :: Key Block -> [PortSpec]
-    connsFrom ps = M.findWithDefault [] ps fromMap
-
-    -- Calculating the post-dominator tree
-
-    tree :: Tree Node
-    tree = pdomTree (exitNode, graph)
-
-    -- Calculating the scopes
-    -- (by traversing the tree)
-
-    scopes = execWriter $ go $ fmap (`IM.lookup` node2obj) tree
-
-    go :: Tree (Maybe (Either (Key Block) PortSpec)) -> Writer [Scope] ()
-    go (Node (Just (Right (ps@(BlockPort blockKey portKey)))) childs) = do
-        mapM_ go childs
-        -- Does this open any scope?
-        unless (null (portScopes port)) $ do
-            tell [(childBlocks childs, ps)]
-      where
-        block = blocks proof M.! blockKey
-        rule = block2Rule ctxt block
-        port = ports rule M.! portKey
-
-        childBlocks childs = [ b | Just (Left b) <- concatMap flatten childs ]
-    go (Node _ childs) = mapM_ go childs -- exit, conclusion and block nodes
