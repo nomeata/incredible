13f387754485c5c17a2e1383bca948da8566e8f4
Joachim Breitner <mail@joachim-breitner.de>
2022-07-04 17:53:38 +0200

    Find more stable JS vendor URLs

--- install-jslibs.sh
+++ install-jslibs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 set -x
@@ -14,14 +14,14 @@ test -d jquery-ui || {
 	rm -f jquery-ui-1.11.4.zip
 	}
 test -e lodash.min.js ||
-	wget -c  http://jointjs.com/js/vendor/lodash/lodash.min.js
+	wget -c https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.min.js
 test -e backbone-min.js ||
-	wget -c  http://jointjs.com/js/vendor/backbone/backbone-min.js
+	wget -c https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.1/backbone-min.js
 test -e joint.min.js ||
-	wget -c http://jointjs.com/cms/downloads/joint.min.js
+	wget -c https://cdnjs.cloudflare.com/ajax/libs/jointjs/0.9.6/joint.min.js
 test -e joint.min.css ||
-	wget -c http://jointjs.com/cms/downloads/joint.min.css
+	wget -c https://cdnjs.cloudflare.com/ajax/libs/jointjs/0.9.6/joint.min.css
 test -e i18next-1.10.1.min.js ||
         wget -c https://raw.githubusercontent.com/i18next/i18next/1.10.1/release/i18next-1.10.1.min.js
 test -e FileSaver.min.js ||
-        wget -c https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.min.js
+        wget -c https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
